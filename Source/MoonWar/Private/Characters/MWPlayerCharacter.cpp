9589de8b59a155e12ff0aaaf198d11aab49b4702
paradise_nisemono <111268812+ParadiseNiseMono@users.noreply.github.com>
2026-04-18 22:00:53 +0800

    Add the Jump Input Action

--- Source/MoonWar/Private/Characters/MWPlayerCharacter.cpp
+++ Source/MoonWar/Private/Characters/MWPlayerCharacter.cpp
@@ -3,3 +3,43 @@
 
 #include "Characters/MWPlayerCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+AMWPlayerCharacter::AMWPlayerCharacter()
+{
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	PlayerCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+}
+
+void AMWPlayerCharacter::PawnClientRestart()
+{
+	Super::PawnClientRestart();
+	
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	if (PC)
+	{
+		UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+
+		if (InputSubsystem)
+		{
+			InputSubsystem->RemoveMappingContext(DefaultMappingContext);
+			InputSubsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+}
+
+void AMWPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (EnhancedInputComponent) {
+		EnhancedInputComponent->BindAction(JumpInputAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+	}
+}
--- Source/MoonWar/Private/Characters/MWPlayerCharacter.h
+++ Source/MoonWar/Private/Characters/MWPlayerCharacter.h
@@ -6,6 +6,10 @@
 #include "Characters/MWCharacter.h"
 #include "MWPlayerCharacter.generated.h"
 
+class USpringArmComponent;
+class UCameraComponent;
+class UInputAction;
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,5 +17,24 @@ UCLASS()
 class AMWPlayerCharacter : public AMWCharacter
 {
 	GENERATED_BODY()
+
+public:
+	AMWPlayerCharacter();
+
+	//~ APawn interface
+	virtual void PawnClientRestart() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> PlayerCamera;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> JumpInputAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
 };
